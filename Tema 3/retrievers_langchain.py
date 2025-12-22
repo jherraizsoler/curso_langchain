0ce9ed9e0327d1ead64c89c1915cc4d4a0971578
Jorge Herraiz Soler <estudiantejorgeherraiz@gmail.com>
2025-12-22 19:46:23 +0100

    Desarrollo funcionalidad multi_query-retriever.py

---
+++ Tema 3/retrievers_langchain.py
@@ -0,0 +1,24 @@
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+
+
+vectorstore = Chroma(
+    embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"),
+    persist_directory="Tema 3\\chroma_db"
+)
+
+retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 2}
+)
+
+consulta = "¿Dónde se encuentra el local del contrato en el que participa María Jiménez Campos?"
+
+resultados = retriever.invoke(consulta)
+
+print("Top 2 documentos mas similares a la consulta: \n")
+
+for i, doc in enumerate(resultados, start=1):
+    print(f"Contenido: {doc.page_content}")
+    print(f"Metadatos: {doc.metadata}")
