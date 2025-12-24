d3f66a3d31ffa7ed3c8aafe9d9d1712d1817405b
Jorge Herraiz Soler <estudiantejorgeherraiz@gmail.com>
2025-12-24 11:34:12 +0100

    Memoria, historial, método no profesional, no adecuado.

---
+++ Tema 5/fundamentos_memoria.py
@@ -0,0 +1,40 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage,AIMessage
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "Eres un asistente útil."),
+    MessagesPlaceholder(variable_name="history"),
+    ("human", "{input}")
+])
+
+chain = prompt | llm
+
+
+history = []
+
+print("Chat en terminal (escribe 'salir' para terminar)\n")
+
+while True:
+    try:
+        user_input = input("Tú: ").strip()
+    except (EOFError, KeyboardInterrupt):
+        print("\nHasta luego!")
+        break
+
+    if not user_input:
+        continue
+    if user_input.lower() in {"salir", "exit", "quit"}:
+        print("Hasta luego!")
+        break
+
+    respuesta = chain.invoke({"history":history, "input": user_input})
+    print("Asistente:", respuesta.content)
+    
+    # Actualizamos el historial de mensajes  (Manera no profesional o no adecuada)
+    history.extend([
+        HumanMessage(content=user_input),
+        AIMessage(content=respuesta.content)
+    ])
